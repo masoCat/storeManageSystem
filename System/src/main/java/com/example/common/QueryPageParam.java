eb2fc7946f69cc1286cc7fa399e06ee5acff0010
masoCat <149858442+masoCat@users.noreply.github.com>
2024-06-18 14:49:29 +0800

    更新 QueryPageParam.java

--- System/src/main/java/com/example/common/QueryPageParam.java
+++ System/src/main/java/com/example/common/QueryPageParam.java
@@ -13,5 +13,5 @@ public class QueryPageParam {
     private int pageSize = PAGE_SIZE;
     private int pageNum = PAGE_NUM;
 
-    private HashMap param;
+    private HashMap param = new HashMap();
 }
