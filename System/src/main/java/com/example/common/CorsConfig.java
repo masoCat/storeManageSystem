5a4551067b6030a69c4fd99629f64d36f3d75d27
masoCat <149858442+masoCat@users.noreply.github.com>
2024-06-18 14:56:36 +0800

    跨域请求

---
+++ System/src/main/java/com/example/common/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.example.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
+}
