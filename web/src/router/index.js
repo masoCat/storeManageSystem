8504c1f3570b5a29279a76936d1c8e118a2d2737
masoCat <149858442+masoCat@users.noreply.github.com>
2024-06-19 17:26:26 +0800

    动态路由

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -15,16 +15,6 @@ const routes = [
                 name: 'HomePage',
                 meta: {title: '首页'},
                 component: () => import('../components/HomePage')
-            }, {
-                path: '/Admin',
-                name: 'admin',
-                meta: {title: '管理员管理'},
-                component: () => import('../components/admin/AdminManage.vue')
-            }, {
-                path: '/User',
-                name: 'user',
-                meta: {title: '用户管理'},
-                component: () => import('../components/user/UserManage.vue')
             }
         ]
     }
@@ -34,6 +24,14 @@ const router = new VueRouter({
     mode: 'history',
     routes
 })
+
+export function resetRouter() {
+    router.matcher = new VueRouter({
+        mode: 'history',
+        routes: []
+    }).matcher
+}
+
 const VueRouterPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(to) {
     return VueRouterPush.call(this, to).catch(err => err)
--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,7 +1,29 @@
 import vue from 'vue'
 import Vuex from 'vuex'
+import router, {resetRouter} from "@/router";
 
 vue.use(Vuex)
+
+function addNewRoute(menuList) {
+
+    let routes = router.options.routes
+
+    routes.forEach(routeItem => {
+        if (routeItem.path == '/Index') {
+            menuList.forEach(menu => {
+                let childRoute = {
+                    path: '/' + menu.menuclick,
+                    name: menu.menuname,
+                    meta: {title: menu.menuname},
+                    component: () => import('../components/' + menu.menucomponent)
+                }
+                routeItem.children.push(childRoute)
+            })
+        }
+    })
+    resetRouter()
+    router.addRoutes(routes)
+}
 
 export default new Vuex.Store({
     state: {
@@ -10,6 +32,8 @@ export default new Vuex.Store({
     mutations: {
         setMenu(state, menuList) {
             state.menu = menuList
+
+            addNewRoute(menuList)
         }
     },
     getters: {
