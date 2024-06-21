8a65ac35ad7d287614f7649c7c878d75d23bf9d3
masoCat <149858442+masoCat@users.noreply.github.com>
2024-06-21 17:35:18 +0800

    左侧边栏持久化

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,6 +1,7 @@
 import vue from 'vue'
 import Vuex from 'vuex'
 import router, {resetRouter} from "@/router";
+import createPersistedState from 'vuex-persistedstate'
 
 vue.use(Vuex)
 
@@ -40,6 +41,7 @@ export default new Vuex.Store({
         getMenu(state) {
             return state.menu
         }
-    }
+    },
+    plugins: [createPersistedState()]
 })
 
