f6ea2272831c7d52c7224128dfbd1af6de02984f
masoCat <149858442+masoCat@users.noreply.github.com>
2024-06-21 16:49:05 +0800

    删除无用导入

--- System/src/main/java/com/example/service/IRecordService.java
+++ System/src/main/java/com/example/service/IRecordService.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.entity.Record;
-import com.example.entity.Storage;
 
 /**
  * <p>
