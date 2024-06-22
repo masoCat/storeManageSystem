34014a70dfcc595a1346c23bc6d78e1da85be197
masoCat <149858442+masoCat@users.noreply.github.com>
2024-06-22 10:40:26 +0800

    登录持久化

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,7 +8,22 @@
 
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  data() {
+    return {
+      user: JSON.parse(sessionStorage.getItem("CurUser"))
+    }
+  },
+  watch: {
+    '$store.state.menu': {
+      handler(val, old) {
+        if (!old && this.user && this.user.no) {
+          this.$store.commit("setMenu", val)
+        }
+      },
+      immediate: true
+    }
+  }
 }
 </script>
 
